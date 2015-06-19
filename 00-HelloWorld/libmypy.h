50ac50e0100c69dad751bdba8f5a2b32e6b428f3
StarNight <zack_pan@alumni.ncu.edu.tw>
2015-06-19 12:31:25 +0800

    Add define related macroes for prevention of duplicated definition.

--- 00-HelloWorld/libmypy.h
+++ 00-HelloWorld/libmypy.h
@@ -1,3 +1,8 @@
+#ifndef __LIBMYPY_H__
+#define __LIBMYPY_H__
+
 #include <Python.h>
 
 PyObject * hello(PyObject *);
+
+#endif
--- 01-HeyMan/libmypy.h
+++ 01-HeyMan/libmypy.h
@@ -1,4 +1,9 @@
+#ifndef __LIBMYPY_H__
+#define __LIBMYPY_H__
+
 #include <Python.h>
 
 PyObject * hello(PyObject *);
 PyObject * heyman(PyObject *, PyObject *);
+
+#endif
